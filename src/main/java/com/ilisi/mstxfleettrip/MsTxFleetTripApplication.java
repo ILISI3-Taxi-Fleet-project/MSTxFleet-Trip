83cc57afb1fe740e847c5beb9a7ba5ae5503db2f
yassine <yassine@gmail.com>
2023-11-05 11:38:04 +0100

    first commit

---
+++ src/main/java/com/ilisi/mstxfleettrip/MsTxFleetTripApplication.java
@@ -0,0 +1,13 @@
+package com.ilisi.mstxfleettrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTxFleetTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTxFleetTripApplication.class, args);
+    }
+
+}
